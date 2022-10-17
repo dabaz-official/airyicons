341dae79c1f5afff14db45b6e6d670797ff2dc72
DabAZ <76861837+dabaz-official@users.noreply.github.com>
2022-10-17 21:37:45 +0800

    Add Header component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
@@ -9,6 +9,7 @@ export default function Home() {
         <link rel="icon" href="/logos/logo-violet-600.svg" />
       </Head>
 
+      <Header />
     </div>
   )
 }
